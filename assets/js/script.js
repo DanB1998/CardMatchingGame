be83a6b5a161d00edf6635740a2e982ce4e3a345
DanB1998 <danbeasley1998@yahoo.co.uk>
2022-02-23 15:21:34 +0000

    docstrings and functions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,47 @@
+/**
+ * This function will run the game.
+ */
 function runGame() {
 }
 
+/**
+ * This function will shuffle the cards. Using shuffle array and then iterating the array through the cards.
+ */
 function shuffleCards() {
 }
 
+/**
+ * This function will flip the cards when the click event listener is triggered on the card.
+ */
 function flipCard() {
 }
 
-function checkCards {
+/**
+ * This function will check the cards match. if they do it will trigger the cards match function. If not it will trigger the non-match function.
+ */
+function checkCards() {
+}
+
+/**
+ * This function will trigger a match. Keeping the cards flipped over and incrementing a match in the score and also incrementing the guesses.
+ */
+function cardsMatch() {
+}
+
+/**
+ * This function will trigger a non-match. Flipping the cards back over and incrementing the guesses score.
+ */
+ function nonMatch() {
+}
+
+/**
+ * This function will increment the amount of guesses a user has made in an attempt to match the cards.
+ */
+ function incrementGuesses() {
+}
+
+/**
+ * This function will increment the amount of correct matches the user has made.
+ */
+ function incrementMatches() {
 }
